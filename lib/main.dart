db8226367bc8d536536e4e6e0bbb4996e71f309f
dev <tommykim1981@gmail.com>
2018-09-12 13:11:14 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+// import 'package:flutter/rendering.dart';
+
+void main(){ 
+  // debugPaintBaselinesEnabled = true;  
+  runApp(new App()); 
+}
